8f415e0f9d7d97cfdc3cfbf3125d1e2a7cbb94ed
Muspi Merol <wjj123345567@icloud.com>
2024-10-31 10:54:10 +0800

    feat: add feature 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <div class="absolute inset-0 grid place-items-center">
   <div class="flex flex-col gap-2" data-sveltekit-preload-code="eager">
     <a href="/design/gen-comments">gen-comments</a>
+    <a href="/design/features/1">feature-1</a>
   </div>
 </div>
