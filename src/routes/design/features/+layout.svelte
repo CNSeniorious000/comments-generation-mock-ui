1c96738abe834369b122aef103e3daff16c3fc4e
Muspi Merol <wjj123345567@icloud.com>
2024-10-31 12:16:47 +0800

    fix(style): centering

--- src/routes/design/features/+layout.svelte
+++ src/routes/design/features/+layout.svelte
@@ -2,8 +2,6 @@
   import Box from "$lib/components/lines/Box.svelte";
 </script>
 
-<div class="h-2em flex flex-row gap-5 text-zinc-7">
-  <Box l t b class="h-fit flex flex-row gap-4 p-4" opacity="0.05">
-    <slot />
-  </Box>
-</div>
+<Box l t b class="h-fit flex flex-row gap-4 p-4 text-zinc-7" opacity="0.05">
+  <slot />
+</Box>
