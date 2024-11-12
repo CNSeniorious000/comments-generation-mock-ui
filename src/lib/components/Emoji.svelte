d9c71b94c88b2715e9f91b4e9434e13500cbc31d
Muspi Merol <me@promplate.dev>
2024-11-12 14:03:37 +0800

    chore: play transition without delay on mount

--- src/lib/components/Emoji.svelte
+++ src/lib/components/Emoji.svelte
@@ -1,18 +1,27 @@
 <script>
-  import { browser } from "$app/environment";
+  import { onMount, tick } from "svelte";
   import { blur, scale } from "svelte/transition";
 
   export let emoji = "";
   export let start = 0.65;
   export let delay = 100;
   export let amount = 1;
+
+  let first = true;
+  let show = false;
+
+  onMount(async () => {
+    show = true;
+    await tick();
+    first = false;
+  });
 </script>
 
 <div class="relative aspect-1 *:absolute">
-  {#if emoji && browser}
+  {#if emoji && show}
     {#key emoji}
-      <div in:blur={{ delay, opacity: 1, amount }} out:blur={{ opacity: 1, amount }}>
-        <div in:scale={{ delay, start }} out:scale={{ start }} class={emoji} />
+      <div in:blur|global={{ delay: first ? 0 : delay, opacity: 1, amount }} out:blur={{ opacity: 1, amount }}>
+        <div in:scale|global={{ delay: first ? 0 : delay, start }} out:scale={{ start }} class={emoji} />
       </div>
     {/key}
   {/if}
