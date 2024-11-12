a6c79028522b3ae74764d57eeebef7252d189de7
Muspi Merol <me@promplate.dev>
2024-11-12 15:20:24 +0800

    feat: add emoji icons to safelist in UnoCSS configuration

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,4 @@
+import icons from "./src/routes/demo/emoji-carousel/icons.json";
 import extractorSvelte from "@unocss/extractor-svelte";
 import { defineConfig, presetIcons, presetTypography, presetUno, transformerDirectives, transformerVariantGroup } from "unocss";
 
@@ -5,6 +6,7 @@ const config = defineConfig({
   extractors: [extractorSvelte()],
   transformers: [transformerVariantGroup(), transformerDirectives()],
   presets: [presetUno(), presetIcons(), presetTypography()],
+  safelist: icons,
 });
 
 export default config;
