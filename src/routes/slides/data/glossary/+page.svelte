7baf3e3ede8b7fe96b452512ce0db2cd8e689333
Muspi Merol <me@promplate.dev>
2024-11-15 10:23:45 +0800

    feat: add glossary data table view

---
+++ src/routes/slides/data/glossary/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import glossary from "../../../data/glossary.json";
+</script>
+
+<div class="fixed inset-0 center">
+  <div class="h-lg w-4xl overflow-scroll b-(1 zinc-2)">
+    <table>
+
+      <thead class="sticky top-0 b-b-(1 zinc-2) bg-white/80 text-left backdrop-blur-xl">
+        <tr class="*:font-550">
+          <th>网语</th>
+          <th>解释</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {#each glossary as { term, definition }}
+          <tr>
+            <td class="ws-nowrap">{term}</td>
+            <td>{definition}</td>
+          </tr>
+        {/each}
+      </tbody>
+
+    </table>
+  </div>
+</div>
+
+<style>
+  th, td {
+    --uno: b-1 b-zinc-2 px-2.5 py-1.5 text-zinc-9 first:b-l-0 last:b-r-0;
+  }
+</style>
